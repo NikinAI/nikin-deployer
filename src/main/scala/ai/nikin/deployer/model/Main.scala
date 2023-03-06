ee55ede4f2a7342242e20b028a03ba8217ce6e4b
Wosin <blind.van@gmail.com>
2023-03-06 01:08:16 +0100

    Add schedule spark deployer.

--- src/main/scala/ai/nikin/deployer/model/Main.scala
+++ src/main/scala/ai/nikin/deployer/model/Main.scala
@@ -6,6 +6,10 @@ import com.coralogix.zio.k8s.client.config.{asynchttpclient, k8sCluster, kubecon
 import com.coralogix.zio.k8s.client.model.K8sNamespace
 import com.coralogix.zio.k8s.client.sparkoperator.definitions.sparkapplication.v1beta2.SparkApplication
 import com.coralogix.zio.k8s.client.sparkoperator.v1beta2.sparkapplications
+import com.coralogix.zio.k8s.client.v1.pods
+import com.coralogix.zio.k8s.client.v1.pods.Pods
+import com.coralogix.zio.k8s.model.core.v1.Pod
+import zio.stream.ZSink
 import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 object Main extends ZIOAppDefault{
@@ -17,10 +21,14 @@ object Main extends ZIOAppDefault{
   val client = config >>> k8s.client.config.asynchttpclient.k8sSttpClient
   val cluster = config >>> k8sCluster
 
-  val sparkApplication = (client ++ cluster) >>> sparkapplications.SparkApplications.live
+  val sparkApplication = (client ++ cluster) >>> Pods.live
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
-    SparkDeployer.getAll(K8sNamespace("default"))
-      .provideLayer(sparkApplication)
+    val sink = ZSink.collectAllToSet[Pod]
 
+    pods.getAll(Some(K8sNamespace("kube-system")))
+      .run(sink)
+      .map(_.toList)
+      .map(println(_))
+      .provide(sparkApplication)
   }
 }
