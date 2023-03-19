cbea8bf259e721c91321660af9bc08d82d94be8f
Wosin <blind.van@gmail.com>
2023-03-19 15:39:19 +0100

    More modelling.

--- src/main/scala/ai/nikin/deployer/interpreter/model/Lake.scala
+++ src/main/scala/ai/nikin/deployer/interpreter/model/Lake.scala
@@ -1,10 +1,22 @@
 package ai.nikin.deployer.interpreter.model
 
+import org.apache.avro.reflect.AvroSchema
 
-case class Lake(name: String) extends Definition
+
+sealed trait AggFunction
+object AggFunction {
+  case object Min extends AggFunction
+  case object Max extends AggFunction
+  case object Avg extends AggFunction
+}
+
+case class Schema(name: String, avroSchema: AvroSchema)
+case class Lake(name: String, schema: Schema) extends Definition
 case class SparkApplication(
                            name: String,
                            inputLake: String,
                            outputLake: String,
-
-                           )
+                           function: AggFunction,
+                           inputColName: String,
+                           outputColName: String
+                           ) extends Definition
